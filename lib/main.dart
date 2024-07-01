eec3ae4124177af48faa781300e233f890b4ea45
C1nd127 <clsalazarc_a@ube.edu.ec>
2024-06-30 20:45:24 -0500

    tp2

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mi primera app'),
+        ),
+        body: const Center(
+          child: Text ('Hola Mundo'),
+        ),
+      )
+    );
+  }
+}
+
+
